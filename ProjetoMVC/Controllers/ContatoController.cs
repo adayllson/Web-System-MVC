1723fb1e2ab410027bd2b78b06e49056f26656ff
adailson-almeida <adayllson.almeida@gmail.com>
2022-11-07 15:43:44 -0300

    implementação do método criar como POST

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoMVC.Context;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Controllers
 {
@@ -17,8 +18,27 @@ namespace ProjetoMVC.Controllers
         }
         public IActionResult Index()
         {
+            //Método que retorna do banco de dados a lista todos os contatos atribuindo a variável.
+            //Retornando o carregamento da View do Index e passando como parâmetro a lista de contatos.
             var contatos = _context.Contatos.ToList();
             return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
         }
 
         
